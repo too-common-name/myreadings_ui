fca075120189e72483880d5ac1eb8fe9d4de3ef6
Daniele Rossi <drossi@redhat.com>
2025-04-09 18:32:43 +0200

    feat: fetch info from backend

---
+++ src/utils/axiosInstance.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { getToken } from '@josempgon/vue-keycloak'; 
+
+
+const baseURL = import.meta.env.VITE_API_URL;
+const instance = axios.create({ baseURL });
+
+
+instance.interceptors.request.use(
+  async config => {
+    const token = await getToken();
+    config.headers['Authorization'] = `Bearer ${token}`;
+    return config;
+  },
+  error => {
+    return Promise.reject(error); 
+  },
+);
+
+export default instance;
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -16,12 +16,12 @@
                     </div>
                     <div>
                         <div class="text-h6">Hello</div>
-                        <div class="text-subtitle-1">[Name], [Surname]</div>
+                        <div class="text-subtitle-1">{{userDetails.firstName}} {{userDetails.lastName}}</div>
                     </div>
                 </div>
             </div>
             <div class="readinglists">
-                <div class="text-h6">[Name]'s Reading Lists</div>
+                <div class="text-h6">{{userDetails.firstName}}'s Reading Lists</div>
                 <div class="text-subtitle-1">[Reading](num)</div>
                 <div class="text-subtitle-1">[Readed](num)</div>
             </div>
@@ -34,10 +34,59 @@
 
 <script setup lang="ts">
 import NavigationBar from '@/components/NavigationBar.vue';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import instance from '@/utils/axiosInstance';
+import { getToken } from '@josempgon/vue-keycloak';
+import { jwtDecode } from "jwt-decode";
+import { useElevation } from 'vuetify/lib/composables/elevation.mjs';
 
-const reviewCount = ref(100);
+const profileImageUrl = ref('https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortRound&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=Blue03&graphicType=Blank&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light');
+const reviewCount = ref(0);
 const showReviews = ref(false);
+const readingBooks = ref([]);
+const userDetails = ref({});
+const reviews = ref([]);
+const readingLists = ref([]);
+
+onMounted(async () => {
+    try {
+        const token = await getToken();
+        const parsedToken = jwtDecode(token);
+
+        const userIdFromToken = parsedToken?.sub;
+
+        if (userIdFromToken) {
+            const userResponse = await instance.get(`/api/v1/users/${userIdFromToken}`);
+
+            if (userResponse.data) {
+                userDetails.value = userResponse.data;
+            } else {
+                console.error('Failed to fetch user details');
+            }
+
+            const reviewsResponse = await instance.get(`/api/v1/reviews/users/${userIdFromToken}`);
+            if (reviewsResponse.data) {
+                reviews.value = reviewsResponse.data;
+                reviewCount.value = reviews.value.length;
+            } else {
+                console.error('Failed to fetch user reviews');
+            }
+        } else {
+            console.error('Could not retrieve user ID from the token.');
+        }
+
+        const readingListsResponse = await instance.get('/api/v1/readinglists');
+        if (readingListsResponse.data) {
+            readingLists.value = readingListsResponse.data;
+        } else {
+            console.error('Failed to fetch reading lists');
+        }
+
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
 
 </script>
 
